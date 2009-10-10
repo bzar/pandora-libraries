dfc9fb56e6c3f2acdd29813eec2e20ecccc2212c
skeezix <skeezix@flotsam-vm.(none)>
2009-10-10 18:51:08 -0400

    Turn bitmask into discrete array refs; thats how code was before I cleaned it up and bugged it. Really would love to remove those global vars and convert it to a bitmask, but another day

--- include/pnd_io_gpio.h
+++ include/pnd_io_gpio.h
@@ -1,18 +1,19 @@
 #ifndef GLES2D_EVENT_H
 #define GLES2D_EVENT_H
 
+// I really want to convert this to be a bitmask, but another day
 typedef enum {
-  pke_pad_up = 1<<0,
-  pke_pad_down = 1<<1,
-  pke_pad_left = 1<<2,
-  pke_pad_right = 1<<3,
-  pke_pad_menu = 1<<4,
-  pke_pad_x = 1<<5,
-  pke_pad_y = 1<<6,
-  pke_pad_a = 1<<7,
-  pke_pad_b = 1<<8,
-  pke_pad_l = 1<<9,
-  pke_pad_r = 1<<10,
+  pke_pad_up =    0,
+  pke_pad_down =  1,
+  pke_pad_left =  2,
+  pke_pad_right = 3,
+  pke_pad_menu =  4,
+  pke_pad_x =     5,
+  pke_pad_y =     6,
+  pke_pad_a =     7,
+  pke_pad_b =     8,
+  pke_pad_l =     9,
+  pke_pad_r =    10,
   pke_pad_max
 } pnd_keytype_e;
 
