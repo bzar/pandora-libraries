38c2c72b43848177fd89328ad86e5ed0ba488105
vimacs <vimacs@pndtest>
2010-01-08 08:19:15 +0100

    modified:   testdata/scripts/op_menu.sh

--- testdata/scripts/op_menu.sh
+++ testdata/scripts/op_menu.sh
@@ -11,7 +11,7 @@ if [ $xpid ]; then
   echo "x is running"
   if [ $killist ]; then
     echo "displaying kill list"
-    pidlist=$(pstree -lpA | grep pnd_run.sh | sed -ne 's/.*-\(.*\)(\([0-9]\+\))/\2\n \1/p' | zenity --list --multiple --column "pid" --column "name" --title "kill" --text "which apps should be killed" | sed 's/|/\n/')
+    pidlist=$(pstree -lpA | grep pnd_run.sh | sed -ne 's/.*-\(.*\)(\([0-9]\+\))/\2\n \1/p' | DISPLAY=:0.0 zenity --list --multiple --column "pid" --column "name" --title "kill" --text "which apps should be killed" | sed 's/|/\n/')
     for PID in $pidlist
     do
       kill -9 $PID
@@ -19,7 +19,7 @@ if [ $xpid ]; then
   else
   echo "starting appfinder"
     xfceuser=$(ps u -C xfce4-session | tail -n1 | awk '{print $1}')
-    sudo -u $xfceuser xfce4-appfinder
+    sudo -u $xfceuser DISPLAY=:0.0 xfce4-appfinder
   fi
 else
   echo "no x, killing all pnd aps so x gets restarted"
