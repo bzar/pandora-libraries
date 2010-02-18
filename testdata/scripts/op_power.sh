331d78ddff6ce1b0d4c15eacc3d160e60caa3b99
vimacs <vimacs@pndtest>
2010-02-18 17:27:56 +0100

    Proper wl initfile name 	modified:   testdata/scripts/op_power.sh

--- testdata/scripts/op_power.sh
+++ testdata/scripts/op_power.sh
@@ -25,7 +25,7 @@ if [ "$1" -le "3" ]; then # button was pressed 1-3sec, "suspend"
       echo 500 > /proc/pandora/cpu_mhz_max
     fi
     hciconfig hci0 up
-    /etc/init.d/S30wlxxx-init start
+    /etc/init.d/S30wlxxx-i/etc/init.d/wl1251-init start
     pidlist=$(pstree -lpA | grep pnd_run.sh | sed -ne 's/.*(\([0-9]\+\))/\1/p')
     for PID in $pidlist
     do
@@ -42,7 +42,7 @@ if [ "$1" -le "3" ]; then # button was pressed 1-3sec, "suspend"
       kill -19 $PID #send SIGSTOP
     done
     hciconfig hci0 down
-    /etc/init.d/S30wlxxx-init stop
+    /etc/init.d/S30wlxxx-i/etc/init.d/wl1251-init stop
     echo 0 > /sys/devices/platform/twl4030-pwm0-bl/backlight/twl4030-pwm0-bl/brightness
     echo 14 > /proc/pandora/cpu_mhz_max
   fi
