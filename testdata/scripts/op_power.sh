305909434be399cc3654c209451013cf08c050a3
Michael Mrozek <EvilDragon@openpandora.de>
2010-09-08 02:10:38 +0200

    op_power.sh: Change low power speed to 125MHz. Should be enough for services and goes down to lowest OPP level.

--- testdata/scripts/op_power.sh
+++ testdata/scripts/op_power.sh
@@ -69,7 +69,7 @@ if [ "$1" -le "2" ]; then # button was pressed 1-2sec, "suspend"
     echo 0 > /sys/devices/platform/twl4030-pwm0-bl/backlight/twl4030-pwm0-bl/brightness
     echo 1 > /sys/devices/platform/omapfb/graphics/fb0/blank
     echo 16 > /sys/class/leds/pandora\:\:power/brightness #dim power LED
-    echo 14 > /proc/pandora/cpu_mhz_max
+    echo 125 > /proc/pandora/cpu_mhz_max
   fi
 elif [ "$1" -ge "3" ]; then #button was pressed 3 sec or longer, shutdown
   xfceuser=$(ps u -C xfce4-session | tail -n1 | awk '{print $1}')
